4984562ac0da8bf818f4b4ce978c979a056380f3
Sama <s.daryanavard.1@research.gla.ac.uk>
2021-05-03 13:52:29 +0100

    semicolon

--- examples/line_follower/bpPlots.py
+++ examples/line_follower/bpPlots.py
@@ -44,7 +44,7 @@ stat=statRaw.astype(np.int16)
 numPredictors=stat[0]
 numLayerNeurons=stat[1::]
 
-for i in range(numPredictors):
+for i in range(3): #numPredictors):
     predictor=bpc.predictor(i+1)
     predictor.plotPredictor(error)
     
@@ -53,49 +53,19 @@ for i in range(numPredictors):
 #%%
     
 nLayers = len(numLayerNeurons)
-
 wch=np.loadtxt('{}weight_distances.tsv'.format(path));# error=errordata[:,0]
-wch1=wch[:,nLayers]
-#wch2=wch[:,1]
-#wch3=wch[:,2]
-#maxW1=max(abs(wch[:,0])); maxW2=max(abs(wch[:,1])); maxW3=max(abs(wch[:,2]));
-#maxW=max(maxW1,maxW2,maxW3)
-#wch1=wch1/maxW1
-#wch2=wch2/maxW2
-#wch3=wch3/maxW3
+wch1=wch[:,nLayers] # only looking at the overal weight change
 
 wchfig=plt.figure('weigth change')
 axe=wchfig.add_subplot(111)
 plt.plot(wch1, color='black', linestyle="-", linewidth=0.5)
-#plt.plot(wch2, color='black', linestyle="--", linewidth=0.5)
-#plt.plot(wch3, color='black', linestyle="-.", linewidth=0.5)
-#plt.grid(which='minor', color='grey', linestyle='--', linewidth=0.2)
-#plt.ylabel('Error Signal')
-#plt.ylim(-0.1, 1.1)
-#plt.yticks([0,1])
-#plt.xticks(np.arange(0,len(wch),250))
-#axe.set_aspect(aspect=len(wch)/(1.2*1.5))
 wchfig.savefig(spath+'weightchange', quality= 100, format='svg', bbox_inches='tight')
 plt.show()
 
 #%%
 
-layer=bpc.layer(1)
+layer=bpc.layer(1) # only looking at the first layer pattern
 layer.plotLayerWeights()
 
-#for j in range(len(numLayerNeurons)):
-#    for k in range(numLayerNeurons[j]):
-#        neuron=bpc.neuron(int(str(j+1)+str(k+1)))
-#        neuron.plotWeights()
-
-#for n in range(len(numLayerNeurons)-1):
-#    layer=bpc.layer(n+1)
-#    layer.plotLayerWeights()
-    
-    
-#%%
-  
-#finalLayer=bpc.finalLayer(len(numLayerNeurons))
-#finalLayer.plotFinalLayerWeights()
 
 
